15437e32f743255a5d7b938ae3530a685d8a9ace
Renzo Oldani <oldanirenzo@gmail.com>
2020-11-24 05:48:31 -0300

    Agregado test para el primer movimiento de blancas

--- __tests__/whites.test.js
+++ __tests__/whites.test.js
@@ -1,3 +1,4 @@
+const { whiteMovements } = require("../blancasConSwitch");
 const matriz = require("../movements/matriz");
 
 let data = {
@@ -24,5 +25,23 @@ describe('Matriz', () => {
                     .toBeDefined()
             }
         }
+    });
+});
+
+describe('Piezas blancas, movimientos.', () => {
+    test('Debe mover el peon 2 posiciones hacia adelante', () => {
+        return whiteMovements(data).then(movimiento => {
+            expect(movimiento).toStrictEqual({
+                action: 'move',
+                data: {
+                    board_id: '9294de41-a6e8-4dc1-be6e-0b4310a8f274',
+                    turn_token: '2a7ed463-8419-4dce-87a7-2490c0ad9d33',
+                    from_row: 12,
+                    from_col: 0,
+                    to_row: 10,
+                    to_col: 0,
+                }
+            })
+        })
     });
 });
