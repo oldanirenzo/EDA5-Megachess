a0fa00c33d24c9f7a52aef76aa1323909fd404d8
Renzo Oldani <oldanirenzo@gmail.com>
2020-11-26 02:16:13 -0300

    Cambio en los test

--- __tests__/blacks.test.js
+++ __tests__/blacks.test.js
@@ -1,4 +1,5 @@
-const { whiteMovements } = require("../movements/blancasConSwitch");
+const { blackMovements } = require("../movements/negrasConSwitch");
+
 
 let data = {
     event: 'your_turn',
@@ -6,28 +7,26 @@ let data = {
         board_id: '9294de41-a6e8-4dc1-be6e-0b4310a8f274',
         turn_token: '2a7ed463-8419-4dce-87a7-2490c0ad9d33',
         username: 'oldanirenzo',
-        actual_turn: 'white',
+        actual_turn: 'black',
         board: 'rrhhbbqqkkbbhhrrrrhhbbqqkkbbhhrrpppppppppppppppppppppppppppppppp                                                                                                                                PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPRRHHBBQQKKBBHHRRRRHHBBQQKKBBHHRR',
-        move_left: 200,
+        move_left: 199,
         opponent_username: 'oldanirenzo'
     }
 }
 
-let whiteMovementsArray = []
 
 
-
-describe('Piezas blancas, Movimientos.', () => {
+describe('Piezas negras, Movimientos', () => {
     test('Debe mover el peon 2 posiciones hacia adelante', () => {
-        return whiteMovements(data).then(movimiento => {
+        return blackMovements(data).then(movimiento => {
             expect(movimiento).toStrictEqual({
                 action: 'move',
                 data: {
                     board_id: '9294de41-a6e8-4dc1-be6e-0b4310a8f274',
                     turn_token: '2a7ed463-8419-4dce-87a7-2490c0ad9d33',
-                    from_row: 12,
+                    from_row: 3,
                     from_col: 0,
-                    to_row: 10,
+                    to_row: 5,
                     to_col: 0,
                 }
             })
@@ -35,7 +34,7 @@ describe('Piezas blancas, Movimientos.', () => {
     });
 
     test('Los from_row, from_col, to_row y to_col deben ser mayores o iguales a 0', () => {
-        return whiteMovements(data)
+        return blackMovements(data)
         .then(movimiento => {
             expect(movimiento.data.from_row).toBeGreaterThanOrEqual(0);
             expect(movimiento.data.from_col).toBeGreaterThanOrEqual(0);
@@ -45,7 +44,7 @@ describe('Piezas blancas, Movimientos.', () => {
     });
 
     test('Los from_row, from_col, to_row y to_col deben ser menores o iguales a 15', () => {
-        return whiteMovements(data)
+        return blackMovements(data)
         .then(movimiento => {
             expect(movimiento.data.from_row).toBeLessThanOrEqual(15);
             expect(movimiento.data.from_col).toBeLessThanOrEqual(15);
@@ -54,4 +53,3 @@ describe('Piezas blancas, Movimientos.', () => {
         })
     });
 });
-
