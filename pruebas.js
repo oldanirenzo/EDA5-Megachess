0039a728d7aaf993fcc31a59ed761bb1c5352081
Renzo Oldani <oldanirenzo@gmail.com>
2020-11-26 02:17:53 -0300

    Realizo distintas pruebas

--- pruebas.js
+++ pruebas.js
@@ -1,4 +1,4 @@
-const { whiteMovements } = require("./blancasConSwitch");
+// const { whiteMovements } = require("./blancasConSwitch");
 const matriz = require("./movements/matriz");
 const decideAction = require("./movements/options");
 
@@ -12,26 +12,26 @@ let data = {
         board_id: '9294de41-a6e8-4dc1-be6e-0b4310a8f274',
         turn_token: '2a7ed463-8419-4dce-87a7-2490c0ad9d33',
         username: 'oldanirenzo',
-        actual_turn: 'white',
-        board: 'rrhhbbqqkkbbhhrrrrhhbbqqkkbbhhrr  p  p       p   p       ppp  p pp pppppp   p            p   p p  ppp ppp  pp ppq         q     Q     Q  Q QQQ   PP P    P     P P P      PP  PPP  PP  PP   PP    P  PP P P          P P      P RRHHBBQQKKBBHHRRRRHHBBQQKKBBHHRR',
+        actual_turn: 'black',
+        // board: 'rrhhbbqqkkbbhhrrrrhhbbqqkkbbhhrr  p  p       p   p       ppp  p pp pppppp   p            p   p p  ppp ppp  pp ppq         q     Q     Q  Q QQQ   PP P    P     P P P      PP  PPP  PP  PP   PP    P  PP P P          P P      P RRHHBBQQKKBBHHRRRRHHBBQQKKBBHHRR',
         // board: 'rrhhbbqqkkbbhhrrrrhhbbqqkkbbhhrr    ppppp p p pp ppppp   p  pp ppp p  ppp     p p          p p           pp       q        q                                    PP PPPPP PPPPPP P    PP     PPP   P     P      P PPPP  PPPPP   PRRHHBBQQKKBBHHRRRRHHBBQQKKBBHHRR',
+        board: 'h     Qq    hhrr   h       qhhr   ppp  p pp ppp                  p                                                                                                                                              PHPPP  P  PPPPP       QH    HHRRH     QQ    H RR',
         move_left: 8,
         opponent_username: 'oldanirenzo'
     }
 }
-// console.log(matriz(data))
-// if (matriz(data)[13][1] === 'P') {
-//     console.log(true)
-// }
-
-
-whiteMovements(data)
-    .then(move => console.log(JSON.stringify(move)))
-    .catch(err => reject(err))
 
 
 
+// whiteMovements(data)
+//     .then(move => console.log(JSON.stringify(move)))
+//     .catch(err => reject(err))
+
+
+// console.log(matriz(data)[0][15])
+// console.log(matriz(data)[14][15])
 // let action = decideAction(data);
+
 decideAction(data)
     .then(res => {
         console.log('RES: ' + JSON.stringify(res))
