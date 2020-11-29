c91febb293a79d4b01e31b39e749bc06abfa1339
Renzo Oldani <oldanirenzo@gmail.com>
2020-11-29 11:58:40 -0300

    Arreglo el movimiento de los peones negros

--- pieces/blackPawn.js
+++ pieces/blackPawn.js
@@ -6,53 +6,48 @@ const blackPawn = async (board, from_row, from_col, enemyColor, eatPiece) => {
             //Movimiento doble si no se ha movido
             if (from_row === 2 || from_row === 3) {
                 pawnMovements.push({
-                    value: 100 + from_row,
+                    value: 50 + from_row,
                     from_row: from_row,
                     from_col: from_col,
                     to_row: from_row + 2,
-                    to_col: from_col,
-                    valuePiece: valuePiece[board[from_row][from_col]]
+                    to_col: from_col
                 })
             }
 
         }
         if (enemyColor.includes(board[from_row + 1][from_col + 1])) {
             pawnMovements.push({
-                value: value[board[from_row + 1][from_col + 1]],
+                value: eatPiece[board[from_row + 1][from_col + 1]],
                 from_row: from_row,
                 from_col: from_col,
                 to_row: from_row + 1,
-                to_col: from_col + 1,
-                valuePiece: valuePiece[board[from_row][from_col]]
+                to_col: from_col + 1
             })
         }
         if (enemyColor.includes(board[from_row + 1][from_col - 1])) {
             pawnMovements.push({
-                value: value[board[from_row + 1][from_col - 1]],
+                value: eatPiece[board[from_row + 1][from_col - 1]],
                 from_row: from_row,
                 from_col: from_col,
                 to_row: from_row + 1,
-                to_col: from_col - 1,
-                valuePiece: valuePiece[board[from_row][from_col]]
+                to_col: from_col - 1
             })
         }
         if (from_row + 1 === 7) {
             pawnMovements.push({
-                value: 500,
+                value: 200,
                 from_row: from_row,
                 from_col: from_col,
                 to_row: from_row + 1,
-                to_col: from_col,
-                valuePiece: valuePiece[board[from_row][from_col]]
+                to_col: from_col
             })
         }
         pawnMovements.push({
-            value: 100 + from_row,
+            value: 50 + from_row,
             from_row: from_row,
             from_col: from_col,
             to_row: from_row + 1,
-            to_col: from_col,
-            valuePiece: valuePiece[board[from_row][from_col]]
+            to_col: from_col
         })
     }
     return pawnMovements;
