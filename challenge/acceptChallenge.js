d26f232ec7c7fb11079c963ce410f9e68ea5eee6
Renzo Oldani <oldanirenzo@gmail.com>
2020-12-08 02:52:42 -0300

    Modifico la manera de aceptar el desafio

--- challenge/acceptChallenge.js
+++ challenge/acceptChallenge.js
@@ -2,28 +2,16 @@
 
 const acceptChallenge = (data) => {
 
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
 
-        acceptOrNot(data)
-            .then(challenge => {
-                resolve(challenge)
-            })
-            .catch(err => reject(err))
-
-    })
-
-}
-
-const acceptOrNot = async (data) => {
-    if (data.event === 'ask_challenge') {
-        return ({
+        resolve({
             action: 'accept_challenge',
             data: {
                 board_id: data.data.board_id
             }
         })
-    }
 
+    })
 }
 
 module.exports = acceptChallenge
