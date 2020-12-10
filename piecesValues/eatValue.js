497e3a27b829d3eef43969917e2e797bb68ac03c
Renzo Oldani <oldanirenzo@gmail.com>
2020-12-09 21:10:01 -0300

    Cambios en el valor de las piezas

--- piecesValues/eatValue.js
+++ piecesValues/eatValue.js
@@ -4,17 +4,17 @@ let eatWhitePiece = {
     H: 300,
     B: 400,
     R: 600,
-    Q: 350,
+    Q: 450,
     K: 1000,
 }
 
 
 let eatBlackPiece = {
     p: 100,
-    h: 300,
+    h: 300, 
     b: 400,
     r: 600,
-    q: 350,
+    q: 450,
     k: 1000,
 }
 
